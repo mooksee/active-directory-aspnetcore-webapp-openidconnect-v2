2beefeb91dad28318a70ad7a3eaabc3e1a43b46d
mooksee <ouamar@outlook.fr>
2024-05-02 17:25:24 +0200

    Delete 2-WebApp-graph-user directory [ci skip]

--- 2-WebApp-graph-user/2-3-Multi-Tenant/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApp_MultiTenant_v2.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- 2-WebApp-graph-user/2-3-Multi-Tenant/Utils/GraphScope.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WebApp_MultiTenant_v2.Utils
-{
-    public static class GraphScope
-    {
-        public const string UserReadAll = "User.Read.All";
-    }
-}
