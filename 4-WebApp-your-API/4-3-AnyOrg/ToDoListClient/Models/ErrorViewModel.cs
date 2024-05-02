70bdcc3b5426ade07c971f01051acceedb1a546f
mooksee <ouamar@outlook.fr>
2024-05-02 17:34:50 +0200

    Delete 4-WebApp-your-API/4-3-AnyOrg directory [ci skip]

--- 4-WebApp-your-API/4-3-AnyOrg/ToDoListClient/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ToDoListClient.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
